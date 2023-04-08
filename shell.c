c12d1b4d7dc1d4fdc5fb5a07dbd235c9b1915493
Mandizvidzafn <ncebamandizvidza@gmail.com>
2023-04-09 01:05:36 +0200

    Simple shell

--- shell.c
+++ shell.c
@@ -1,25 +1,42 @@
-#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-/**
- * main - Entry point for the shell
- *
- * Return: Always 0
- */
 int main(void)
 {
-        char *input;
-        int status;
+    char *line = NULL;
+    size_t len = 0;
+    ssize_t read;
+    char *args[] = {NULL}; // array of arguments to pass to execve
 
-        do {
-                printf("#cisfun$ ");
-                input = _getline();
-                if (input == NULL)
-                {
-                        printf("\n");
-                        exit(0);
-                }
-                status = execute(input);
-                free(input);
-        } while (status);
-        return (0);
+    while (1) {
+        printf("#cisfun$ "); // display prompt
+        read = getline(&line, &len, stdin);
+        if (read == -1) { // handle end of file condition
+            printf("\n");
+            break;
+        }
+        line[read - 1] = '\0'; // replace newline with null terminator
+
+        pid_t pid = fork();
+        if (pid == -1) { // handle fork error
+            perror("Error");
+            exit(EXIT_FAILURE);
+        }
+        if (pid == 0) { // child process
+            if (execve(line, args, NULL) == -1) { // execute command
+                perror("Error");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else { // parent process
+            int status;
+            waitpid(pid, &status, 0); // wait for child process to finish
+        }
+    }
+
+    free(line);
+    exit(EXIT_SUCCESS);
 }
+
