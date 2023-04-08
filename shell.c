57271513628bd42ff5690c1e4c9cbfc10462f28e
Mandizvidzafn <ncebamandizvidza@gmail.com>
2023-04-09 00:56:44 +0200

    Simple shell

--- shell.c
+++ shell.c
@@ -2,28 +2,54 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
 #include <sys/wait.h>
 
-int main(void)
+#define MAX_INPUT_SIZE 1024
+#define MAX_TOKEN_SIZE 64
+#define MAX_NUM_TOKENS 64
+
+int main()
 {
-    char *buffer = NULL;
-    size_t bufsize = 0;
+    char *tokens[MAX_NUM_TOKENS];
+    char input[MAX_INPUT_SIZE];
+    char *cwd;
+    pid_t pid;
+    int status;
 
-    while (1)
-    {
-        printf("#cisfun$ ");
-        if (getline(&buffer, &bufsize, stdin) == -1)
-        {
-            printf("\n");
+    while (1) {
+        cwd = getcwd(NULL, 0);
+        printf("%s$ ", cwd);
+        free(cwd);
+
+        if (!fgets(input, MAX_INPUT_SIZE, stdin))
             break;
-        }
 
-        char *command = strtok(buffer, " \n");
-        if (command == NULL)
+        int num_tokens = 0;
+        char *token = strtok(input, " \n");
+        while (token != NULL && num_tokens < MAX_NUM_TOKENS) {
+            tokens[num_tokens] = token;
+            token = strtok(NULL, " \n");
+            num_tokens++;
+        }
+        tokens[num_tokens] = NULL;
+
+        if (num_tokens == 0)
             continue;
 
-        pid_t pid = fork();
-        if (pid == -1)
-        {
-            perror("Error");
+        pid = fork();
+        if (pid < 0) {
+            perror("fork failed");
+            exit(1);
+        } else if (pid == 0) {
+            execvp(tokens[0], tokens);
+            perror("execvp failed");
+            exit(1);
+        } else {
+            waitpid(pid, &status, 0);
+        }
+    }
+
+    return 0;
+}
 
